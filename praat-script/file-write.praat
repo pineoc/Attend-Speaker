7b332013b644d47179a3f24b276cd95e28fed664
이윤석 <pineoc@naver.com>
2016-04-20 22:03:54 +0900

    praat script bridge function added

--- praat-script/file-write.praat
+++ praat-script/file-write.praat
@@ -1,4 +1,33 @@
+
+#script execute command
+#praat.exe file-write.praat -20 4 0.4 0.1 no "./sound-data" "test1.wav"
+
+#file read operate
+
+#read command line params
+form Test command line calls
+    	integer first_int -20
+    	real second_real 0.4
+    	real third_real 0.4
+    	real fourth_real 0.1
+    	sentence bool_text no
+    	sentence datadir_text ./sound-data
+	sentence dataname_text test1.wav
+endform
+
+#if sentence, add $ to tail to param_name
+#if not sentence, remove 
+writeFileLine: "test1-t", "first real: ", first_int
+appendFileLine: "test1-t", "second real: ", second_real
+appendFileLine: "test1-t", "third real: ", third_real
+appendFileLine: "test1-t", "fourth real: ", fourth_real
+appendFileLine: "test1-t", "book sentence: ", bool_text$
+appendFileLine: "test1-t", "data_dir sentence: ", datadir_text$ + "/" + dataname_text$
+appendFileLine: "test1-t", "data_name sentence: ", dataname_text$
+
+
+
 #file write to squares.txt numbers
-for i to 100
-    appendFileLine: "squares.txt", "The square of ", i, " is ", i * i
-endfor
+#for i to 100
+#    appendFileLine: "squares.txt", "The square of ", i, " is ", i * i
+#endfor
--- routes/index.js
+++ routes/index.js
@@ -64,11 +64,36 @@ router.get('/exec-dir', function(req, res, next){
         env: process.env
     });
 });
+
 /*
-* praat execute test
-*
-*
-* */
+ * ls execute using spawn
+ *
+ *
+ * */
+router.get('/spawn-ls', function(req, res){
+    const spawn = require('child_process').spawn;
+    const ls = spawn('ls', ['-lh', '/usr']);
+
+    ls.stdout.on('data', function(data) {
+        console.log('stdout: ', data);
+        res.send("data: " + data);
+    });
+
+    ls.stderr.on('data', function(data){
+        console.log('stderr: ', data);
+        res.send("error");
+    });
+
+    ls.on('close', function(code) {
+        console.log('child process exited with code ', code);
+    });
+});
+
+/*
+ * praat execute test
+ *
+ *
+ * */
 router.get('/exec-praat', function(req, res){
     var childProcess = require('child_process');
 
@@ -90,23 +115,78 @@ router.get('/exec-praat', function(req, res){
     var scriptParameters = "-20 4 0.4 0.1 no \"" + workingDir + "\"   \"" + wavFile + "\"";
 
     //command
-    var textGridCommand = praatCommand + __dirname + "/../praat-script/" + scriptToRun + "  " + scriptParameters; //+ " 2>&1 ";
-
-    childProcess.exec(textGridCommand , function(error, stdout, stderr) {
-        if(error){
-            console.log('error occurred, ', error);
-            res.send('error');
-        } else {
-            console.log('std: ', stdout, stderr);
-            console.log("Script execution is complete, here are the results: " + stdout);
-            res.send('success');
-        }
-    }, {
+    var textGridCommand = praatCommand + __dirname + "/../praat-script/" + scriptToRun + " " + scriptParameters; //+ " 2>&1 ";
+    //console.log("textGridCommand: ", textGridCommand);
+    var options = {
         encoding: 'utf8',
         timeout: 0,
         killSignal: 'SIGTERM',
         cwd: praat_dir,
         env: process.env
+    };
+    childProcess.exec(textGridCommand , options, function(error, stdout, stderr) {
+        if(error){
+            console.log('error occurred, ', error);
+            res.send('error');
+        } else {
+            console.log('std: ', stdout, stderr);
+            res.send('exec success');
+        }
+    });
+});
+
+/*
+* execute praat using spawn.
+* it has no stdout
+* it just end
+* */
+router.get('/spawn-praat', function(req, res){
+    var childProcess = require('child_process');
+    const spawn = childProcess.spawn;
+
+    //praat command path
+    //praat.exe file should download to project home directory
+    var praatCommand = path.join(__dirname, "..") + "/praat.exe";
+    var praatMacCommand = " Applications/Praat.app/Contents/MacOS/Praat ";
+
+    //c:\Users\Administrator\Desktop\capstone\attend-speaker
+    var praat_dir = path.join(__dirname, '..');
+
+    //script for praat script function
+    var scriptToRun = "file-write.praat";
+    //sound file working directory
+    var workingDir = "./sound-data";
+    //sound file
+    var wavFile = "test1.wav";
+    //script params
+    var scriptParameters = "-20 4 0.4 0.1 no \"" + workingDir + "\"   \"" + wavFile + "\"";
+    var scriptDir = __dirname + "/../praat-script/" + scriptToRun;
+    var scriptParams = [-20, 4, 0.4, 0.1, "no", workingDir, wavFile];
+
+    //command
+    var textGridCommand = praatCommand + __dirname + "/../praat-script/" + scriptToRun + " " + scriptParameters; //+ " 2>&1 ";
+    //console.log("textGridCommand: ", textGridCommand);
+    var options = {
+        encoding: 'utf8',
+        timeout: 0,
+        killSignal: 'SIGTERM',
+        cwd: praat_dir,
+        env: process.env
+    };
+    const praat = spawn(praatCommand, [scriptDir, scriptParameters], options);
+
+    praat.stdout.on('data', function(data) {
+        console.log('stdout: ', data);
+        res.send("praat data: " + data);
+    });
+
+    praat.stderr.on('data', function(data){
+        console.log('stderr: ', data);
+        res.send("error");
+    });
+
+    praat.on('close', function(code) {
+        console.log('child process exited with code ', code);
     });
 });
 
