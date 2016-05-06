a7f6967479e8987f95a72ae7a32bcd5009595482
이윤석 <pineoc@naver.com>
2016-05-06 19:49:59 +0900

    test code delete

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,21 @@ router.get('/', function(req, res, next) {
     res.render('mainPage', { title: 'mainPage' });
 });
 
+/* GET register user page */
 router.get('/register-speaker', function(req, res, next) {
     res.render('record', {title: 'recordPage'});
 });
 
-/*
- * show check attendance page
- *
- *
- * */
+/* GET show check attendance page */
 router.get('/check-attendance', function(req, res, next) {
     res.render('check', {title: 'checkPage'});
 });
 
-
+/*
 router.get('/exec-test', function(req, res, next){
     var child_proc = require('child_process');
-    var comp_dir = path.join(__dirname, '../data-compare-sol/data-compare-sol/x64/Debug');
-    var cmd_file = path.join(__dirname, '../data-compare-sol/data-compare-sol/x64/Debug') + "/data-compare-sol.exe";
+    var comp_dir = path.join(__dirname, '../data-compare-sol');
+    var cmd_file = path.join(__dirname, '../data-compare-sol') + "/data-compare-sol.exe";
     cmd_file = cmd_file.replace(/\\/g, '/');
     console.log('cmd_file: ', cmd_file);
     var exec_callback = function(err, stdout, stderr){
@@ -51,46 +48,6 @@ router.get('/exec-test', function(req, res, next){
     };
     child_proc.exec(cmd_file, options, exec_callback);
 });
-
-/*
-//test db user insert
-router.post('/user-insert-test', function(req, res){
-    var dbController = require('./dbController');
-    dbController.insertOneUser(["c:/Users/test/Documents/GitHub/Attend-Speaker/routes/../sound-data/jiyoon1234/", "jiyoon", "1234"], function(result){
-        if(result.resCode){
-            console.log("result: ", result);
-            res.json({resCode: 1, msg: "insert success"});
-        } else {
-            //error on db insert
-            console.log("error msg: ", result.msg);
-            res.json({resCode: -1, msg: "insert fail"});
-        }
-    });
-});
 */
-
-router.get('/comp-test', function(req,res){
-    var praatCont = require('./praatConnector');
-    var user_path = __dirname + "/../sound-data/jiyoon1234";
-    var filename1 = "jiyoon1.wav";
-    var filename2 = "jiyoon2.wav";
-    praatCont.compareDatas(user_path + "/", filename1, filename2, function(result){
-        if(result.resCode == 1){
-            //success
-            console.log('comp success, data: ', result, result);
-            res.json({
-                result: "success",
-                pitch_rate: result.pitch_rate,
-                int_rate: result.int_rate,
-                isValid: result.isValid
-            });
-        } else {
-            //fail
-            console.log('comp fail, data: ', result);
-            res.send('fail');
-        }
-    });
-});
-
 
 module.exports = router;
